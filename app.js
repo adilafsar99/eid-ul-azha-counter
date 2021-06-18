8ca9e2718c58caefca34fc85dcb8f66e7e3dc149
Muhammad Adil <adilafsar99@gmail.com>
2021-06-18 18:33:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //Getting difference between today and Eid day in milliseconds
-var today = new Date().getTime();
+
 var eidDay = new Date("20 July 2021").getTime();
-var diffInMillis = eidDay - today;
-var days = Math.floor(diffInMillis / (1000 * 3600 * 24));
-var hours = Math.floor(diffInMillis / (1000 * 3600)) % 24;
-var minutes = Math.floor(diffInMillis / (1000 * 60)) % 60;
+var now = new Date().getTime();
+var diffInMillis = eidDay - now;
+
+    
+var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+//var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
 //Displaying the remaining time in the browser
 document.getElementById("days").innerText = days + " ";
